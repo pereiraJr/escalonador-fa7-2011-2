b2e0cb9d123d82debf0ed9dac383bf8c2d1c3d8d
nayalison@gmail.com <nayalison@gmail.com@d4bdb5db-29b3-6546-ae7a-d7cb49af7892>
2011-10-16 20:56:10 +0000

    Correção no layout

--- trunk/Escalonador/src/br/com/escalonador/controller/Controller.java
+++ trunk/Escalonador/src/br/com/escalonador/controller/Controller.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
-import javax.swing.SwingUtilities;
 
 import br.com.escalonador.model.Estado;
 import br.com.escalonador.model.Processo;
